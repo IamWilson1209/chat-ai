f692b83d99587daa6e7801dc7c9044b36d867d23
WeiShiuan <zenfonlee@gmail.com>
2025-01-12 07:44:11 +0800

    fix domain issue

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       /* add .convex.cloud */
+//       { hostname: "dev:basic-ibis-688.convex.cloud" },
+//       { hostname: "oaidalleapiprodscus.blob.core.windows.net" },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
+module.exports = {
   images: {
     remotePatterns: [
-      /* add .convex.cloud */
-      { hostname: "dev:basic-ibis-688.convex.cloud" },
-      { hostname: "oaidalleapiprodscus.blob.core.windows.net" },
+      {
+        protocol: 'https',
+        hostname: 'basic-ibis-688.convex.cloud',
+      },
     ],
   },
-};
-
-export default nextConfig;
+}
