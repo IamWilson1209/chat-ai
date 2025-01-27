724bfb8a8caffcdda7aaaa224f40df52cf0ab041
WeiShiuan <zenfonlee@gmail.com>
2025-01-27 15:43:01 +0800

    fix css h-[calc(<>-<>)]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import RightDashboard from '@/components/home/(right-dashboard)/page';
 export default function Home() {
   return (
     <main className="m-5">
-      <div className="flex overflow-y-hidden h-[calc(100vh-50px) max-w-[1700px] mx-auto bg-left-dashboard">
+      <div className="flex overflow-y-hidden h-[calc(100vh-50px)] max-w-[1700px] mx-auto bg-left-dashboard">
         {/* background decorator for Light Mode */}
         <div className="fixed top-0 left-0 w-full h-36 dark:bg-transparent -z-30" />
         <LeftDashboard />
