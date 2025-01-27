613af9faf2901b2ce4c85bc82d577898b9e9e607
WeiShiuan <zenfonlee@gmail.com>
2025-01-27 15:22:17 +0800

    redux provider

--- src/providers/store-provider.tsx
+++ src/providers/store-provider.tsx
@@ -1,12 +1,10 @@
-import { Provider } from 'react-redux';
-import { store } from '../app/redux/stores';
-import '../styles/globals.css';
-import type { AppProps } from 'next/app'; // 引入 Next.js 預設型別
+// providers/redux-provider.tsx
+'use client'; // 如果使用 Next.js，確保這是 Client Component
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
-  );
-}
+import { ReactNode } from 'react';
+import { Provider } from 'react-redux';
+import { store } from '@/app/redux/stores/index'; // 根據你的實際路徑調整
+
+export const ReduxProvider = ({ children }: { children: ReactNode }) => {
+  return <Provider store={store}>{children}</Provider>;
+};
