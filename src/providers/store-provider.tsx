beae600b31cbe0545f96f4987caee9495c170f81
WeiShiuan <zenfonlee@gmail.com>
2025-01-27 15:47:05 +0800

    add comment

--- src/providers/store-provider.tsx
+++ src/providers/store-provider.tsx
@@ -1,9 +1,9 @@
 // providers/redux-provider.tsx
-'use client'; // 如果使用 Next.js，確保這是 Client Component
+'use client';
 
 import { ReactNode } from 'react';
 import { Provider } from 'react-redux';
-import { store } from '@/app/redux/stores/index'; // 根據你的實際路徑調整
+import { store } from '@/app/redux/stores/index';
 
 export const ReduxProvider = ({ children }: { children: ReactNode }) => {
   return <Provider store={store}>{children}</Provider>;
